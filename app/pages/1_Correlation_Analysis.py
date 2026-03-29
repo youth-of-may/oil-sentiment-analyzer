286bb0c82a3791ee5f595c22ebbdaaa7843af66d
youth-of-may <135508335+youth-of-may@users.noreply.github.com>
2026-03-29 18:13:14 +0800

    Created new pages for correlation analysis and news_explorer

---
+++ app/pages/1_Correlation_Analysis.py
@@ -0,0 +1,11 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent / "src"))
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+st.set_page_config(page_title="Geopolitical Sentiment & Oil Price Correlation", page_icon="⛽", layout="wide")
